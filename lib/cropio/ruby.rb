e8fc710141176c52be06fd8edcf0d34ec18e2d07
Sergey Vernidub <svernidub@gmail.com>
2015-07-22 20:04:10 +0300

    Initial commit.

---
+++ lib/cropio/ruby.rb
@@ -0,0 +1,7 @@
+require "cropio/ruby/version"
+
+module Cropio
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ spec/cropio/ruby_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Cropio::Ruby do
+  it 'has a version number' do
+    expect(Cropio::Ruby::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
