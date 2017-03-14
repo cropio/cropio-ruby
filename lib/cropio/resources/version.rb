098d05285b8c5b415c6fd85b3130f20f861e4ce9
Yaroslav Nelin <y.nelin@gmail.com>
2017-03-14 08:40:06 +0200

    New resources from Cropio (#3)

---
+++ lib/cropio/resources/version.rb
@@ -0,0 +1,6 @@
+module Cropio
+  module Resources
+    class Version < Cropio::Resource::Base
+    end
+  end
+end
---
+++ lib/cropio/resources/work_record_machine_region_mapping_item.rb
@@ -0,0 +1,6 @@
+module Cropio
+  module Resources
+    class WorkRecordMachineRegionMappingItem < Cropio::Resource::Base
+    end
+  end
+end
