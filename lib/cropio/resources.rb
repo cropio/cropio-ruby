d4fc2076b96be46dc55afbbe37b36961bdf26823
Yaroslav Nelin <y.nelin@gmail.com>
2020-08-27 10:49:03 +0300

    Add FuelHourlyDataItem model

--- lib/cropio/resources.rb
+++ lib/cropio/resources.rb
@@ -62,7 +62,7 @@ require_relative './resources/land_document_land_parcel_mapping_item'
 require_relative './resources/land_parcel'
 require_relative './resources/protected_document'
 require_relative './resources/productivity_estimate_history'
-
+require_relative './resources/fuel_hourly_data_item'
 
 module Cropio
   module Resources
--- lib/cropio/version.rb
+++ lib/cropio/version.rb
@@ -1,3 +1,3 @@
 module Cropio
-  VERSION = '0.18'.freeze
+  VERSION = '0.19'.freeze
 end
