98c64404ab2fb08dabf84ffbdccd9cc452562ba3
Yarik <y.nelin@gmail.com>
2023-11-06 10:33:59 +0200

    Improve fetching changes from CWO for period

--- lib/cropio/resource/base.rb
+++ lib/cropio/resource/base.rb
@@ -117,14 +117,15 @@ module Cropio
           limit -= limit < LIMIT ? limit : LIMIT
           to_time = last_record_time(response) || from_time
 
-          # if there is one record in time range we need to step back
-          # for 1ms to exclude cycling
-          if response['data'].count == 1
-            to_time = (Time.parse(to_time) - 1/999999.0).iso8601(6)
-          end
           buffer += response['data']
+
+          break if obtained_records_less_limit(response)
         end
         buffer
+      end
+
+      def self.obtained_records_less_limit(response)
+        response['meta']["response"]["obtained_records"] < response['meta']["response"]["limit"]
       end
 
       def self.last_record_time(response)
